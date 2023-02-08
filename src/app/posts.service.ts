6c79a7ecb2e4a72e50b2fa74b12daf988366492b
Aga <aga@MacBook-Pro-Aga.local>
2023-02-08 11:49:16 +0100

    first init for http

---
+++ src/app/posts.service.ts
@@ -0,0 +1,69 @@
+import { Post } from './post.model';
+import { HttpClient, HttpEventType, HttpHeaderResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core"
+import { map, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root'})
+export class PostsService {
+  error = new Subject<string>();
+
+  constructor(private http: HttpClient){
+  }
+
+  createAndStorePost(title: string, content: string){
+    const postsData: Post = { title: title, content: content};
+    this.http.post<{name: string}>(
+      'https://ng-complete-guide-6bf78-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+      postsData, {
+        observe: 'response'
+      }
+      ).subscribe(
+        responseData => {
+          console.log(responseData);
+        }, error => {
+          this.error.next(error.message);
+        }
+      );
+  }
+
+
+  fetchPosts(){
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('cutom', 'key');
+  return this.http.get<{[key: string]: Post}>(
+        'https://ng-complete-guide-6bf78-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        {
+          headers: new HttpHeaders({"Custom-Header": 'Hello'}),
+          params: searchParams,
+          responseType: 'json'
+        })
+        .pipe(map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id: key})
+            }
+          }
+          return postsArray;
+        }))
+    }
+
+  deletePosts(){
+    return this.http.delete(
+      'https://ng-complete-guide-6bf78-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+      {
+        observe: 'events',
+        responseType: 'text'
+      }).pipe(tap(event => {
+        console.log(event)
+        if(event.type === HttpEventType.Sent){
+        ///
+        }
+        if(event.type === HttpEventType.Response){
+          console.log(event.body);
+        }
+      }));
+  }
+}
